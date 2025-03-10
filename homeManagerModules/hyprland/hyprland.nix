6beb18f9cef57efccbb9121903f44767bf7cddcb
fakesproink <bsushi308@gmail.com>
2025-03-10 21:57:38 +0700

    added window moving and resizing

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -170,7 +170,11 @@
           "$mainMod SHIFT, =, movetoworkspace, 12"
           "$mainMod, mouse_down, workspace, e+1"
           "$mainMod, mouse_up, workspace, e-1"
+        ];
+
+        bindm = [
           "$mainMod, mouse:272, movewindow"
+          "$mainMod, mouse:273, resizewindow"
         ];
 
         input = {
