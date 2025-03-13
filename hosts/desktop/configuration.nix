4c060f001b777d789c49dabde288d03f4b356b85
fakesproink <bsushi308@gmail.com>
2025-03-13 14:15:49 +0700

    added a plymouth boot screen

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -51,7 +51,21 @@
 
   services.udisks2.enable = true;
   services.gvfs.enable = true;
-  boot.kernelParams = [ "quiet" ];
+  boot = {
+    plymouth.enable = true;
+
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    loader.systemd-boot.consoleMode = "max";
+  };
 
   nixpkgs.config.packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
