80fc8f1a6109479c5b66a9dec99e7245e7e37a70
fakesproink <bsushi308@gmail.com>
2025-04-05 21:42:17 +0700

    changed wallpaper again asfasdfasdfasdfsadfsadf also added new fastfetch

--- nixosModules/stylix.nix
+++ nixosModules/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    image = ./../homeManagerModules/hyprland/wallpaper/wj08pcekrpzb1.png;
+    image = ./../homeManagerModules/hyprland/wallpaper/ign_Firewatch-2.png;
     polarity = "dark";
     autoEnable = true;
 
