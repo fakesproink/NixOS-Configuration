d4bcec81baa40c44d67b731747901d9daff92c03
fakesproink <bsushi308@gmail.com>
2025-04-09 17:14:47 +0700

    updated screenshots + cava

--- homeManagerModules/cava/cava.nix
+++ homeManagerModules/cava/cava.nix
@@ -1,12 +1,16 @@
-{ pkgs, lib, config, ... }: {
+{ lib, config, ... }: {
   options = {
     cava.enable =
       lib.mkEnableOption "cava";
   };
 
   config = lib.mkIf config.cava.enable {
-    home.packages = with pkgs; [
-      cava
-    ];
+    programs.cava = {
+      enable = true;
+      settings = {
+        general.framerate = 60;
+        color.foreground = "'#88c0d0'";
+      };
+    };
   };
 }
