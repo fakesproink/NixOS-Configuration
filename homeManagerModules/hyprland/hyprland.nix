97189142eaa61c17add70c295e2f3e845ecdd58b
fakesproink <bsushi308@gmail.com>
2026-04-01 22:08:20 +0700

    turned off middle click to paste

--- homeManagerModules/hyprland/hyprland.nix
+++ homeManagerModules/hyprland/hyprland.nix
@@ -117,6 +117,7 @@
         misc = {
           force_default_wallpaper = 0;
           disable_hyprland_logo = true;
+          middle_click_paste = false;
         }; 
 
         "$mainMod" = "SUPER";
